74da57f1e1956ffbac7e44ef06e3ba7d52b9b9e4
TensorTom <14287229+TensorTom@users.noreply.github.com>
2020-12-22 04:18:22 -0500

    Added another test.

--- tests/test_shorten.py
+++ tests/test_shorten.py
@@ -4,12 +4,19 @@ import uuid
 
 
 async def test_shorten():
+	test_url = f"https://example.com/"
+	short_url = await unu.shorten(url=test_url, output_format="json", keyword="")
+	assert 'statusCode' in short_url
+	assert short_url.statusCode == 200
+	assert short_url.shorturl == 'https://u.nu/example'
+
+
+async def test_shorten_unique():
 	test_url = f"https://example.com/?test={uuid.uuid4()}"
 	short_url = await unu.shorten(url=test_url, output_format="json", keyword="")
 	assert 'statusCode' in short_url
 	assert short_url.statusCode == 200
-	assert short_url.shorturl == test_url
 
 
-asyncio.run(test_shorten())
+
 
